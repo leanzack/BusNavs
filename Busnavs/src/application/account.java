7cf06f1c4fded26ffab2e3a8778de77979c6c982
leanzack <138343724+leanzack@users.noreply.github.com>
2024-04-28 13:34:42 +0800

    Second push

--- Busnavs/src/application/account.java
+++ Busnavs/src/application/account.java
@@ -10,11 +10,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class account extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/edit.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/acc.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
--- Busnavs/src/module-info.java
+++ Busnavs/src/module-info.java
@@ -3,6 +3,8 @@ module Busnavs {
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires java.desktop;
+	requires java.sql;
+
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
